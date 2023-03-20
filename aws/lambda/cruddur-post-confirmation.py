e3f4a6134b478b072aec2aeab44ecb78582952c2
madhavi-chavva <mailtomemadhavi2023@gmail.com>
2023-03-20 19:55:32 +0000

    modified lambda and gitpod files

--- aws/lambda/cruddur-post-confirmation.py
+++ aws/lambda/cruddur-post-confirmation.py
@@ -4,16 +4,15 @@ import os
 
 def lambda_handler(event, context):
     user = event['request']['userAttributes']
-    print("userAttributes")
+    print('userAttributes')
     print(user)
 
-    user_display_name = user['name']
-    user_email        = user['email']
-    user_handle       = user['preferred_username']
-    user_cognito_id   = user['sub']
-
+    user_display_name  = user['name']
+    user_email         = user['email']
+    user_handle        = user['preferred_username']
+    user_cognito_id    = user['sub']
     try:
-      print('entered-try')  
+      print('entered-try')
       sql = f"""
          INSERT INTO public.users (
           display_name, 
@@ -21,23 +20,23 @@ def lambda_handler(event, context):
           handle, 
           cognito_user_id
           ) 
-        VALUES(
-          '{user_display_name}', 
-          '{user_email}',
-          '{user_handle}',
-          '{user_cognito_id}'
-        )
-      """ 
-      print("SQL statement ---------")
+        VALUES(%s,%s,%s,%s)
+      """
+      print('SQL Statement ----')
       print(sql)
       conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
       cur = conn.cursor()
-      cur.execute(sql)
+      params = [
+        user_display_name,
+        user_email,
+        user_handle,
+        user_cognito_id
+      ]
+      cur.execute(sql,*params)
       conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
       print(error)
-        
     finally:
       if conn is not None:
           cur.close()
