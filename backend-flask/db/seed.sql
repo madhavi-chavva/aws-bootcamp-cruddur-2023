c20c03f3feaa68f4fd878914b8be7c58545117c5
madhavi-chavva <mailtomemadhavi2023@gmail.com>
2023-03-27 16:09:16 +0000

    implement conversion dynamodb

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,12 +1,12 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'MOCK'),
+  ('Madhavi Chavva', 'mailtomemadhavi2023@gmail.com', 'madhu' ,'MOCK'),
   ('Andrew Bayko', 'bayko@exampro.co', 'bayko' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'madhu' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
