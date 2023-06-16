3b05eabccc3d35224cf62f9c44afa767effcfd87
madhavi-chavva <mailtomemadhavi2023@gmail.com>
2023-06-16 20:55:31 +0000

    seed another user into rds and modify Activityitem for props.expanded

--- frontend-react-js/src/components/ActivityItem.js
+++ frontend-react-js/src/components/ActivityItem.js
@@ -5,13 +5,38 @@ import ActivityActionReply  from '../components/ActivityActionReply';
 import ActivityActionRepost  from '../components/ActivityActionRepost';
 import ActivityActionLike  from '../components/ActivityActionLike';
 import ActivityActionShare  from '../components/ActivityActionShare';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
+import { useNavigate  } from "react-router-dom";
 
 export default function ActivityItem(props) {
+  const navigate = useNavigate()
+
+  const click = (event) => {
+    event.preventDefault()
+    const url = `/@${props.activity.handle}/status/${props.activity.uuid}`
+    navigate(url)
+    return false;
+  }
+
+  let expanded_meta;
+  if (props.expanded === true) {
+    expanded_meta = '1:56 PM · May 23, 2023';
+  }
+
+
+  const attrs = {}
+  let item
+  if (props.expanded === true) {
+    attrs.className = 'activity_item expanded'
+  } else {
+    attrs.className = 'activity_item clickable'
+    attrs.onClick = click
+  }
   return (
-    <Link className='activity_item' to={`/@${props.activity.handle}/status/${props.activity.uuid}`}>
+    <div {...attrs}>
       <div className="acitivty_main">
         <ActivityContent activity={props.activity} />
+        {expanded_meta}
         <div className="activity_actions">
           <ActivityActionReply setReplyActivity={props.setReplyActivity} activity={props.activity} setPopped={props.setPopped} activity_uuid={props.activity.uuid} count={props.activity.replies_count}/>
           <ActivityActionRepost activity_uuid={props.activity.uuid} count={props.activity.reposts_count}/>
@@ -19,6 +44,6 @@ export default function ActivityItem(props) {
           <ActivityActionShare activity_uuid={props.activity.uuid} />
         </div>
       </div>
-    </Link>
-  );
+    </div>
+  )
 }
